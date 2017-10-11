30b8c8c818d30e4cfd00b3ed74a8fc7629478c27
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-10-11 10:51:07 +0200

    Add integrity signature
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,18 @@ extern crate base64;
 extern crate futures;
 extern crate hex;
 extern crate openssl;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate walkdir;
 
 pub mod error;
+mod integrity;
 mod keys;
 mod package;
 
 pub use package::sign_package;
+pub use integrity::sign_app;
 
 #[cfg(test)]
 mod tests {
