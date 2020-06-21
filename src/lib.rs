9de16f18e198916ac60b22c2b0f4505a501ffe2a
Robin Appelman <robin@icewind.nl>
2020-06-21 22:50:36 +0200

    switch from failure to std:error::Error
    failure is depricated and doesn't integrate well with other crates not using it

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
-mod keys;
 mod package;
+
+#[cfg(test)]
+mod test;
+
+use openssl::error::ErrorStack;
+use std::io;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+#[non_exhaustive]
+pub enum Error {
+    #[error("Failed to load key file: {0}")]
+    FailedToLoadKey(#[source] io::Error),
+    #[error("Failed to package file: {0}")]
+    FailedToPackage(#[source] io::Error),
+    #[error("Key could not be parsed by openssl: {0}")]
+    InvalidKey(#[source] ErrorStack),
+    #[error("Failed to sign package: {0}")]
+    SignatureFailed(#[source] ErrorStack),
+}
 
 pub use package::sign_package;
